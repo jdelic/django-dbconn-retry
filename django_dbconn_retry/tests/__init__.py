4f002a8ce2fa8b718bb3afb159ea163c8ca32290
Jonas Maurus <jonas-github@maurus.net>
2018-01-21 16:02:04 +0100

    add django test project

--- django_dbconn_retry/tests/__init__.py
+++ django_dbconn_retry/tests/__init__.py
@@ -1,4 +1,7 @@
 # -* encoding: utf-8 *-
+from unittest.mock import Mock
+
+import django_dbconn_retry as ddr
 
 from django.test import TestCase
 
@@ -6,3 +9,13 @@ from django.test import TestCase
 class ReconnectTests(TestCase):
     def test_getting_root(self):
         self.client.get('/')
+
+    def test_prehook(self):
+        cb = Mock()
+        ddr.add_pre_reconnect_hook(cb)
+        cb.assert_called()
+
+    def test_posthook(self):
+        cb = Mock()
+        ddr.add_post_reconnect_hook(cb)
+        cb.assert_called()
