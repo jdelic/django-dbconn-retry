f275c7c79fa3d6e645beb8e75c4d8c621c99868d
Jonas Maurus <jonas-github@maurus.net>
2018-01-21 16:40:05 +0100

    postgresql testing setup for travisci

--- django_dbconn_retry/tests/__init__.py
+++ django_dbconn_retry/tests/__init__.py
@@ -1,6 +1,8 @@
 # -* encoding: utf-8 *-
 from unittest.mock import Mock
 
+from django.db import connection
+
 import django_dbconn_retry as ddr
 
 from django.test import TestCase
@@ -13,9 +15,13 @@ class ReconnectTests(TestCase):
     def test_prehook(self):
         cb = Mock()
         ddr.add_pre_reconnect_hook(cb)
+        connection.close()
+        self.client.get('/')
         cb.assert_called()
 
     def test_posthook(self):
         cb = Mock()
         ddr.add_post_reconnect_hook(cb)
+        connection.close()
+        self.client.get('/')
         cb.assert_called()
--- django_dbconn_retry/tests/test_project/test_project/settings.py
+++ django_dbconn_retry/tests/test_project/test_project/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbconn',
+        'USER': 'postgres',
+        'PASSWORD': '',
     }
 }
 
