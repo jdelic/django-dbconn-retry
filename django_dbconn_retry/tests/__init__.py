9fbf77cd292b7d0a88a0087b74b1a9115ec0c9c9
Jonas Maurus <jonas-github@maurus.net>
2018-01-21 23:33:26 +0100

    try to force connections to be closed for tests

--- django_dbconn_retry/tests/__init__.py
+++ django_dbconn_retry/tests/__init__.py
@@ -3,7 +3,7 @@ from unittest.mock import Mock
 
 import django_dbconn_retry as ddr
 
-from django.db import connection
+from django.db import connection, OperationalError
 from django.test import TestCase
 
 
@@ -14,13 +14,14 @@ class ReconnectTests(TestCase):
     def test_prehook(self) -> None:
         cb = Mock()
         ddr.add_pre_reconnect_hook(cb)
-        connection.close()
-        self.client.get('/')
+        connection.closed = property(lambda: True, lambda: None)
+        self.assertRaises(OperationalError, self.client.get('/'))
         cb.assert_called()
 
     def test_posthook(self) -> None:
         cb = Mock()
         ddr.add_post_reconnect_hook(cb)
-        connection.close()
-        self.client.get('/')
+        connection.closed = property(lambda: True, lambda: None)
+        self.assertRaises(OperationalError, self.client.get('/'))
         cb.assert_called()
+
