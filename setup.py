4cdac2b95868b0b54332629b8377693f09a41202
Jonas Maurus <jonas-github@maurus.net>
2018-01-21 22:29:25 +0100

    add tests to sdist so they can be executed on travisci

--- setup.py
+++ setup.py
@@ -13,8 +13,11 @@ except IOError:
 
 setup(
     name="django-dbconn-retry",
-    version="0.1.1",
+    version="0.1.2",
     packages=["django_dbconn_retry"],
+    package_dir={
+        '': '.',
+    },
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
