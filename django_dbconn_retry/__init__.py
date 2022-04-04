7ef970a24af1020a1b54d371334d16cbb0e26f20
Jonas Maurus <jonas-github@maurus.net>
2022-04-05 00:39:01 +0200

    export the interface and events from apps

--- django_dbconn_retry/__init__.py
+++ django_dbconn_retry/__init__.py
@@ -2,5 +2,8 @@
 import django
 
 
+from django_dbconn_retry.apps import pre_reconnect, post_reconnect, monkeypatch_django
+
+
 if django.VERSION < (3, 2):
     default_app_config = 'django_dbconn_retry.DjangoIntegration'
