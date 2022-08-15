e3fa5fc813d38307e50765ca1b351ef65812c73b
Jonas Maurus <jonas-github@maurus.net>
2022-08-16 00:47:50 +0200

    for older Django actually export the DjangoIntegration AppConfig

--- django_dbconn_retry/__init__.py
+++ django_dbconn_retry/__init__.py
@@ -2,10 +2,10 @@
 import django
 
 
-from django_dbconn_retry.apps import pre_reconnect, post_reconnect, monkeypatch_django
+from django_dbconn_retry.apps import pre_reconnect, post_reconnect, monkeypatch_django, DjangoIntegration
 
 
-__all__ = [pre_reconnect, post_reconnect, monkeypatch_django]
+__all__ = [pre_reconnect, post_reconnect, monkeypatch_django, DjangoIntegration]
 
 if django.VERSION < (3, 2):
     default_app_config = 'django_dbconn_retry.DjangoIntegration'
