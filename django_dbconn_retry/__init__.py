2ac640095ded2ce0d8616df88e44c85917ae380f
Jonas Maurus <jonas-github@maurus.net>
2026-05-02 17:42:30 +0200

    remove Django < 3.2 compatibility shim

--- django_dbconn_retry/__init__.py
+++ django_dbconn_retry/__init__.py
@@ -6,5 +6,3 @@ from django_dbconn_retry.apps import pre_reconnect, post_reconnect, monkeypatch_
 
 __all__ = [pre_reconnect, post_reconnect, monkeypatch_django, DjangoIntegration]
 
-if django.VERSION < (3, 2):
-    default_app_config = 'django_dbconn_retry.DjangoIntegration'
